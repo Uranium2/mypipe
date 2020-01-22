cf4ce26bf745e144a03aef9c3adf709d8e1483f2
Tavernier Antoine <tavernier.antoine@hotmail.fr>
2020-01-22 12:28:47 +0100

    Compiles, but main panicked

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate clap;
 use clap::{Arg, App};
-use std::process::Command;
+use std::io::{self, Write};
+
 
 fn main() {
     let matches = App::new("mypipe")
@@ -25,15 +26,31 @@ fn main() {
     let input_arg = matches.value_of("input").unwrap();
     let output_arg = matches.value_of("output").unwrap();
 
-    let mut input_cmd : std::process::Command;
-    let mut output_cmd : std::process::Command;
+    let input_cmd = if cfg!(target_os = "windows") {
+        std::process::Command::new("cmd")
+                .args(&["/C", input_arg])
+                .output()
+                .expect("failed to execute process")
+    } else {
+        std::process::Command::new("sh")
+                .arg("-c")
+                .arg(input_arg)
+                .output()
+                .expect("failed to execute process")
+    };
 
-    if cfg!(target_os = "windows") {
-        input_cmd = std::process::Command::new("cmd");
-        output_cmd = std::process::Command::new("cmd");
-    }
-    if cfg!(target_os = "linux") {
-        input_cmd = std::process::Command::new("sh");
-        output_cmd = std::process::Command::new("sh");
-    }
+    let output_cmd = if cfg!(target_os = "windows") {
+        std::process::Command::new("cmd")
+                .args(&["/C", &String::from_utf8(input_cmd.stdout).unwrap()])
+                .output()
+                .expect("failed to execute process")
+    } else {
+        std::process::Command::new("sh")
+                .arg("-c")
+                .arg(&String::from_utf8(input_cmd.stdout).unwrap())
+                .output()
+                .expect("failed to execute process")
+    };
+    
+
 }
